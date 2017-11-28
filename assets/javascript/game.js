37cd5cd53ed4518ca8739721bda66a5e406528e9
Marylin <marylin@Marylins-MacBook-Pro.local>
2017-11-27 18:59:58 -0600

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var randomWords= ["What Happens in Vegas Stays in Vegas", "Bellagio", "MGM Grand", "Chippendales", "Thunder Down Under", "Mandalay Bay", "Excalibur", "Luxor", "Cosmopolitan", "High Roller", "The Mirage", "Planet Hollywood", "Stratosphere", "Circus Circus", "Siegfried and Roy", "Jabbawockeez", "Penn and Teller", "The Strip", "Fremont Street Experience", "Treasure Island", "Caesars Palace", "The Venetian", "Cirque du Soleil", "Palazzo"];
+
+var WinNumber= 0;
+
+var guesses= 15;
