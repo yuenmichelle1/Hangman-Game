67c137fbcf8f13f0c9f576d6baa703726206b458
Marylin <marylin@Marylins-MacBook-Pro.local>
2017-12-01 15:44:07 -0600

    adding changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,95 @@
-document.onkeyup= function(event) { 
-	alert("Let's play Hangman!");
-}
-var randomWords= ["What Happens in Vegas Stays in Vegas", "Bellagio", "MGM Grand", "Chippendales", "Thunder Down Under", "Mandalay Bay", "Excalibur", "Luxor", "Cosmopolitan", "High Roller", "The Mirage", "Planet Hollywood", "Stratosphere", "Circus Circus", "Siegfried and Roy", "Jabbawockeez", "Penn and Teller", "The Strip", "Fremont Street Experience", "Treasure Island", "Caesars Palace", "The Venetian", "Cirque du Soleil", "Palazzo"];
+//document.onkeypress= function(KeyPress) { //needs to cover the whole code!
+//	document.reset(); -> do not use yet. want: when key is pressed after win or loss, then game will reset 
+//}
 
+var randomWords= ["what happens in vegas stays in vegas", "bellagio", "mgm grand", "chippendales", "thunder down under", "mandalay bay", "excalibur", "luxor", "cosmopolitan", "high roller", "the mirage", "planet hollywood", "stratosphere", "circus circus", "siegfried and roy", "jabbawockeez", "penn and teller", "the strip", "fremont street experience", "treasure island", "caesars palace", "the venetian", "cirque du soleil", "palazzo"];
+//wordBank
 var computerPhrase= randomWords[Math.floor(Math.random()*randomWords.length)];
 //computerPhrase will show up in HTML
-var letterFiller= ""; //either _ or a space, then filled with letter
-for (i=0; i<computerPhrase.length; i++) {
+var spaceFiller= ""; //either _ or a space, then filled with letter
+for (var i=0; i<computerPhrase.length; i++) {
 	var letter= computerPhrase.charAt(i);
 	if (letter === " ") {
-		letterFiller += "&nbsp &nbsp &nbsp";
+		spaceFiller += " ";
 	} else {
-		letterFiller += " _ ";
+		spaceFiller += "-";
 	}
 } 
-//chosen answer will either have a space or if it is a letter will be filled with _
-document.getElementById("hold").innerHTML = letterFiller;
-
-
-var WinNumber= 0;
-
-if (letterFiller !== " _ ") {
-	WinNumber++;
-}
-
 var guessesNumber= 14;
-//if we guess the wrong letter then guessesNumber goes down
-//guessesNumber--
+var wrongGuesses = [];
+var winNumber= 0;
 
-document.getElementById("guesses-left").innerHTML = guessesNumber;
+document.onkeypress = function(keyPress) {
+	if (spaceFiller === computerPhrase && guessesNumber > 0) {
+		winNumber= winNumber+1;
+		document.getElementById("wins").innerHTML=winNumber;
+	}
+
+
+
+	document.onkeyup = function(event){
+	    console.log(event);
+	    var playerChoice= event.key.toLowerCase();
+	    var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	    if (alphabet.includes(playerChoice) === false) {
+	    	alert(`Please pick a letter!`);
+	    } else if(wrongGuesses.includes(playerChoice) === true) {
+	    	alert(`You already guessed that letter!`)
+	    } else if (computerPhrase.indexOf(playerChoice)<0) {
+	    	guessesNumber= guessesNumber -1;
+			wrongGuesses.push(playerChoice);
+			document.getElementById("guesses-left").innerHTML = guessesNumber;
+			document.getElementById("usedLetters").innerHTML = wrongGuesses;
+	    } else{
+	    spaceFiller = guessLetter(playerChoice, spaceFiller, computerPhrase );
+	    document.getElementById("hold").innerHTML =spaceFiller;
+		}
+		if (spaceFiller === computerPhrase && guessesNumber > 0) {
+		winNumber= winNumber+1;
+		document.getElementById("wins").innerHTML=winNumber;
+		}
+		if (guessesNumber === 0) {
+				alert(`You lost!`);
+		}
+
+	}
+
+
+	document.getElementById("wins").innerHTML = winNumber;
+
+	function alterAt ( n, c, originalString ) {
+	    return originalString.substr(0,n) + c + originalString.substr(n+1,originalString.length);
+	}
+
+	function guessLetter( letter, shown, answer ) {
+	    var checkIndex = 0;
+	    
+	    checkIndex = answer.indexOf(letter);
+	    while ( checkIndex >= 0 ) {
+	        shown = alterAt( checkIndex, letter, shown );
+	        checkIndex = answer.indexOf(letter, checkIndex + 1);
+	    }
+	    return shown;
+	}
+
+
+
+	//chosen answer will either have a space or if it is a letter will be filled with _
+	document.getElementById("hold").innerHTML = spaceFiller;
+
+
+	//if we guess the wrong letter then guessesNumber goes down
+
+
+
+
+	document.getElementById("guesses-left").innerHTML = guessesNumber;
+
+	//filler for reset when there is new word 
+
+
+	document.getElementById("usedLetters").innerHTML = wrongGuesses;
+	//filler for reset when there is new word
+
+}
 
